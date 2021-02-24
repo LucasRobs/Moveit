f57ee1c466930bcce397a69819f23d9463080e57
LucasRobs <lucasrobs@alu.ufc.br>
2021-02-23 23:24:49 -0300

    feat: jornada INFINITA

---
+++ moveit-next/src/components/Countdown.tsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+
+import styles from '../styles/components/Countdown.module.css'
+
+export function Countdown(){
+  const [time, setTime] = useState(23*60);
+  const [ative, setActive] = useState(false);
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2,'0').split('');
+  const [secondLeft, secondRight] = String(seconds).padStart(2,'0').split('');
+
+  function startCountdown(){
+    setActive(!ative);
+  }
+
+  useEffect(()=>{
+    if(ative && time > 0){
+      setTimeout(()=>{
+        setTime(time-1)
+      },1000)
+    }
+  }, [ative, time])
+
+  return(
+    <div>
+      <div className={styles.CountdownContainer}>
+        <div>
+          <span>{minuteLeft}</span>
+          <span>{minuteRight}</span>
+        </div>
+        <span>:</span>
+        <div>
+          <span>{secondLeft}</span>
+          <span>{secondRight}</span>
+        </div>
+      </div>
+
+      <button type="button" className={styles.countdownButton} onClick={startCountdown}>
+      Iniciar um ciclo
+      </button>
+    </div>
+  )
+}
--- moveit-next/src/components/Profile.tsx
+++ moveit-next/src/components/Profile.tsx
@@ -1,10 +1,13 @@
+import styles from '../styles/components/Profile.module.css';
 export function Profile(){
   return (
-    <div>
+    <div className={styles.profileContainer}>
       <img src="https://github.com/LucasRobs.png" alt="LucasRobs"/>
       <div>
         <strong>Lucas Robs</strong>
-        <p>Level 1000</p>
+        <p>
+          <img src="icons/level.svg" alt="level"/>
+          Level 1000</p>
       </div>
     </div>
     )
