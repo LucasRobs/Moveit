2631bb654f210c18ef732d0ca3045dfab6a6b34e
LucasRobs <lucasrobs@alu.ufc.br>
2021-02-23 22:06:11 -0300

    feat: migracao

---
+++ moveit-next/src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile(){
+  return (
+    <div>
+      <img src="https://github.com/LucasRobs.png" alt="LucasRobs"/>
+      <div>
+        <strong>Lucas Robs</strong>
+        <p>Level 1000</p>
+      </div>
+    </div>
+    )
+}
